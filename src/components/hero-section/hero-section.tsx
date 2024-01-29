adcf640d761951419c500fa7d8700947d385aa84
Dino Fejzagić <dino.f@live.de>
2024-01-29 21:24:10 +0100

    Styles

--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -3,6 +3,7 @@ import styles from './hero-section.module.css';
 import HeadingTitle from "../typo/heading-title";
 import Paragraph from "../typo/paragraph";
 import keyVisualSrc from './key-visual.jpg';
+import logoSrc from './logo-toolkit.png';
 import Button from "../layout/button";
 
 const HeroSection: FunctionComponent = () => {
@@ -12,12 +13,20 @@ const HeroSection: FunctionComponent = () => {
             <div className={`absolute w-full h-full ${styles.overlay}`}></div>
 
             <div className="absolute w-full h-full flex flex-col justify-center items-center p-8">
+                <div>
+                    <img className="w-32 mb-16" src={logoSrc} />
+                </div>
                 <div className="max-w-4xl text-center">
                     <HeadingTitle level={2}>XR development made easy</HeadingTitle>
-                    <Paragraph>Reality Toolkit, a comprehensive framework meticulously designed to expedite and enhance XR application and game development within the Unity ecosystem.</Paragraph>
+                    <Paragraph>Reality Toolkit, a comprehensive open-source framework designed to expedite and enhance XR application and game development within the Unity ecosystem.</Paragraph>
                 </div>
-                <div>
+                <div className="flex gap-8">
                     <Button to="/docs/get-started">Get Started</Button>
+                </div>
+                <div className="flex gap-8 mt-8">
+                    <a href="https://github.com/realitycollective/com.realitytoolkit.core" target="_blank">GitHub</a>
+                    <a href="https://openupm.com/packages/com.realitytoolkit.core/" target="_blank">OpenUPM</a>
+                    <a href="https://github.com/realitycollective/com.realitytoolkit.core/blob/main/LICENSE.md">License</a>
                 </div>
             </div>
         </div>
