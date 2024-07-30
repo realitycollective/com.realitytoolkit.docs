fd3443adc285d96e9a7feac66a6efb48005e6f9f
SimonDarksideJ <darkside@zenithmoon.com>
2024-07-30 16:16:52 +0100

    Update Team history text

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -32,7 +32,7 @@ export default function Sponsors(): JSX.Element {
                 </div>
 
                 <Title level={3}>Acknowledgements</Title>
-                <Text>The Reality Toolkit was originally based on the MRTK (Mixed Reality Toolkit), created by Microsoft as a successor to the popular HoloToolkit. While the Reality Toolkit took its own path eversince and does things differently in many areas, we'd like to recognize a few people who have left their signature on its journey:</Text>
+                <Text>The Reality Toolkit was originally based on the MRTK (Mixed Reality Toolkit v2), created by Microsoft and MVP's, as a successor to the popular HoloToolkit. Even though the Reality Toolkit has taken its own path and does things differently in many areas, we would like to recognize a few people who have left their signature on its journey:</Text>
                 <ul>
                     <li><a href='https://github.com/stephenhodgson' title='Stephen Hodgson'><Text compact>Stephen Hodgson</Text></a></li>
                     <li><a href='https://github.com/Cangi' title='Rony Portelli'><Text compact>Rony Portelli</Text></a></li>
